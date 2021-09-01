2317c0d9d6c7623b1ac5d687157514e86b08dd74
Luis Felipe Fabiane <luisffabiane@gmail.com>
2021-08-31 22:07:44 -0300

    Update aparelho.py
    Nesse commit foi adicionada a base para a classe em questão

---
+++ aparelho.py
@@ -0,0 +1,5 @@
+class Aparelho:
+    def __init__(self, nome, restricaoIdade, nPessoasTurno):
+        self.nome = nome
+        self.restricaoIdade = restricaoIdade
+        self.nPessoasTurno = nPessoasTurno

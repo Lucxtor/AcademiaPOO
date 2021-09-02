8d6c55150d0ebc5d6cad6cc4586391d37d2ad2ed
Luis Felipe Fabiane <luisffabiane@gmail.com>
2021-09-02 01:02:45 -0300

    Grade de horarios
    Escopo para a utilização da grade de horários com possibilidade da elaboração dos limite de pessoas por dia e turno

--- academia.py
+++ academia.py
@@ -5,16 +5,31 @@ from aparelho import Aparelho
 class Academia:
  
     
-    def __init__(self, nome, capacidadeMaxima, aparelhos=[], professores=[], alunos=[]):
+    def __init__(self, nome, capacidadeMaximaLocal, aparelhos=[], professores=[], alunos=[]):
         self.nome = nome
-        self.capacidadeMaxima = capacidadeMaxima
+        self.capacidadeMaximaLocal = capacidadeMaximaLocal
         self.aparelhos = aparelhos
         self.professores = professores
         self.alunos = alunos
         self.listaAlunos = []
         self.listaProfessores = []
+        self.capacidadeMaximaDiaTurno = [[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0]]
+        # auxCapacidadeMaximaProfessores = 0
+        # auxCapacidadeMaximaAparelhos = 0
+        # for i in aparelhos:
+        #     auxCapacidadeMaximaAparelhos += int(i.nPessoasTurno)
+        # for i in ['M', 'T', 'N']:
+        #    for j in ['seg', 'ter', 'qua', 'qui', 'sex', 'sab']:
+        #         for p in professores:
+        #             for ip in p.diasTrabalho:
+        #                 for jp in p.turnosTrabalho:
+        #                     if i == ip and j == jp:
+        #                         auxCapacidadeMaximaProfessores += 8
+        #         for a in aparelhos:
+        #             auxCapacidadeMaximaAparelhos += int(a.nPessoasTurno)
 
     def entrar(self, nome):
+        pass
         #Verificar se é aluno ou professor, fazendo uma busca nas listas de aluno e professor
             #Se estiver, verificar se alunosDentro+1 é <= do que professoresDentro*8 e se alunosDentro+1 < aparelhos 
                 #Se for, adicionar +1 em alunos dentro e imprimir 'Entrada realizada com sucesso'
@@ -26,6 +41,7 @@ class Academia:
                 #Se não, imprimir mensagem 'Professor não cadastrado'
 
     def cadastrarAluno(self, aluno):
+        pass
         #Verificar se nos dias e turnos desejados, alunosCadastrados+1 <= professoresCadastrados*8 e se alunosCadastrados+1 <= aparelhos
             #Se for, conclui o cadastro dos alunos e imprime 'Aluno cadastrado com sucesso'
             #Se não for, imprime 'Impossível cadastrar nesses dias e horários. Tente novamente'

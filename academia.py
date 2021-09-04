32e94cdc22413b274282e5f21b955a7053698cc7
olavoancay2802 <olavoancay2802@gmail.com>
2021-09-04 12:29:26 -0300

    Adicionando comentários no código

--- academia.py
+++ academia.py
@@ -52,52 +52,53 @@ class Academia:
                     print('\nSaída realizada com sucesso!')                                                  #Imprime mensagem "Saída realizada com sucesso!"
                     print(f'Bom descanso prof. {nome}!')                                                     #Imprime mensagem "Bom descanso {nome}!"
 
-    def cadastrarAluno(self, nome, idade, CPF, horario, peso, altura, objetivo):
-        maxAparelhos = 0
-        cadastroAluno = True
-        for i in self.aparelhos:
-            maxAparelhos += 1
-            if idade < 16 and i.restricaoIdade:
-                maxAparelhos -= 1
-        for i in range(6):
-            for j in range(3):
+    def cadastrarAluno(self, nome, idade, CPF, horario, peso, altura, objetivo):                             #Método de cadastro de aluno, recebendo como parâmetros nome, idade, CPF, horario, peso, altura, objetivo
+        maxAparelhos = 0                                                                                     #Variável usada pra verificar quantos aparelhos o aluno pode usar 
+        cadastroAluno = True                                                                                 #Por padrão, o cadastro é válido
+        for i in self.aparelhos:                                                                             #Repetir(tamanho da lista de aparelhos)vezes linha 59-61
+            maxAparelhos += 1                                                                                #Adiciona 1 a variável maxAparelhos
+            if idade < 16 and i.restricaoIdade:                                                              #Verifica se a idade do aluno é menor do q 16 e se o aparelho tem restrição de idade
+                maxAparelhos -= 1                                                                            #Decrementa em 1 a variável maxAparelhos
+        for i in range(6):                                                                                   #Repete 6 vezes linha 63-66
+            for j in range(3):                                                                               #Repete 3 vezes linha 64-66
                 if horario[i][j] + self.qtdeAlunosDiaTurno[i][j] > self.qtdeProfsDiaTurno[i][j] * 8 or horario[i][j] + self.qtdeAlunosDiaTurno[i][j] > maxAparelhos or horario[i][j] + self.qtdeAlunosDiaTurno[i][j] > self.capacidadeMaximaLocal:
-                    cadastroAluno = False
+                                                                                                             #Verifica se o horário escolhido pelo aluno é válido, comparando com o número de profs em cada horário, número de aparelhos e capacidade do local           
+                    cadastroAluno = False                                                                    #Se for inválido, torna o cadastro inválido
 
-        if cadastroAluno:
-            for i in range(6):
-                for j in range(3):
-                    self.qtdeAlunosDiaTurno[i][j] += horario[i][j]
-            dicionarioAluno = {'Nome': nome, 'tipoPessoa': 'Aluno'}
-            a = Aluno(nome, idade, CPF, horario, peso, altura, objetivo)
-            self.listaAlunos.append(a)
-            self.listaPessoas.append(dicionarioAluno)
-            a.imprimir()
-            if idade < 16:
-                print("\nO aluno cadastrado não pode utilizar os seguintes aparelhos: ")
-                for i in self.aparelhos:
-                    if i.restricaoIdade:
-                        print(i.nomeAparelho)
-            print('\nAluno cadastrado com sucesso')
-            return a
+        if cadastroAluno:                                                                                    #Se passar pelas verificações
+            for i in range(6):                                                                               #Repete 6 vezes linha 70-86
+                for j in range(3):                                                                           #Repete 3 vezes linha 71-86
+                    self.qtdeAlunosDiaTurno[i][j] += horario[i][j]                                           #Adiciona os horários escolhidos pelo aluno na matriz de horários de alunos      
+            dicionarioAluno = {'Nome': nome, 'tipoPessoa': 'Aluno'}                                          #Criação de dicionário com nome do aluno e tipo de pessoa = Aluno
+            a = Aluno(nome, idade, CPF, horario, peso, altura, objetivo)                                     #Instancia um aluno
+            self.listaAlunos.append(a)                                                                       #Adiciona o aluno instanciado a listade alunos
+            self.listaPessoas.append(dicionarioAluno)                                                        #Adiciona o dicionário criado a lista de pessoas
+            a.imprimir()                                                                                     #Invoca o método imprimir para o aluno instanciado
+            if idade < 16:                                                                                   #Verifica se a idade do aluno é <16
+                print("\nO aluno cadastrado não pode utilizar os seguintes aparelhos: ")                     #Caso a idade seja <16, imprime a mensagem "O aluno cadastrado não pode utilizar os seguintes aparelhos: "
+                for i in self.aparelhos:                                                                     #Repete(tamanho da lista de aparelhos)vezes linha 80-81
+                    if i.restricaoIdade:                                                                     #Verifica se o aparelho tem restrição
+                        print(i.nomeAparelho)                                                                #Caso tenha, imprime o nome do aparelho
+            print('\nAluno cadastrado com sucesso')                                                          #Imprime a mensagem "Aluno cadastrado com sucesso"
+            return a                                                                                         #Retorna o aluno instanciado
 
-        else:
-            print('Impossível cadastrar nesses dias e horários. Tente novamente')
+        else:                                                                                                #Caso o cadastro tenha sido inválidado
+            print('Impossível cadastrar nesses dias e horários. Tente novamente')                            #Imprime a mensagem "Impossível cadastrar nesses dias e horários. Tente novamente"
 
-    def cadastrarProfessor(self, nome, idade, CPF, horario, numeroCarteira):
-        p = Professor(nome, idade, CPF, horario, numeroCarteira)
-        for i in range(6):
-            for j in range(3):
-                self.qtdeProfsDiaTurno[i][j] += horario[i][j]
-        self.listaProfessores.append(p)
-        dicionarioProfessor = {'Nome': nome, 'tipoPessoa': 'Professor'}
-        self.listaPessoas.append(dicionarioProfessor)
-        p.imprimir()
-        print('\nProfessor cadastrado com sucesso!')
-        return p
+    def cadastrarProfessor(self, nome, idade, CPF, horario, numeroCarteira):                                 #Método de cadastro de professor, recebendo como parâmetros nome, idade, CPF, horario, numeroCarteira
+        p = Professor(nome, idade, CPF, horario, numeroCarteira)                                             #Instancia um professor
+        for i in range(6):                                                                                   #Repete 6 vezes linha 91-92
+            for j in range(3):                                                                               #Repete 3 vezes linha 92
+                self.qtdeProfsDiaTurno[i][j] += horario[i][j]                                                #Adiciona horários escolhidos pelo professor na matriz de horário de profs
+        self.listaProfessores.append(p)                                                                      #Adiciona o professor instanciado na lista de professores
+        dicionarioProfessor = {'Nome': nome, 'tipoPessoa': 'Professor'}                                      #Criação de dicionário com nome do professor e tipo de pessoa = Professor
+        self.listaPessoas.append(dicionarioProfessor)                                                        #Adiciona o dicionário criado na lista de pessoas
+        p.imprimir()                                                                                         #Invoca o método impriimir do professor instanciado
+        print('\nProfessor cadastrado com sucesso!')                                                         #Imprime a mensagem "Professor cadastrado com sucesso"
+        return p                                                                                             #Retorna o professor instanciado
 
-    def cadastrarAparelho(self, nomeAparelho, restricaoIdade):
-        a = Aparelho(nomeAparelho, restricaoIdade)
-        self.aparelhos.append(a)
-        print("\nAparelho cadastrado com sucesso!")
-        return a
+    def cadastrarAparelho(self, nomeAparelho, restricaoIdade):                                               #Método de cadastro de aparelho, recebendo como parâmetro o nome do aparelho e restrição de idade
+        a = Aparelho(nomeAparelho, restricaoIdade)                                                           #Instancia aparelho
+        self.aparelhos.append(a)                                                                             #Adiciona o aparelho instanciado na lista de aparelhos
+        print("\nAparelho cadastrado com sucesso!")                                                          #Imprime mensagem "Aparelho cadastrado com sucesso!"
+        return a                                                                                             #Retorna o aparelho instanciado
--- mainAcademia.py
+++ mainAcademia.py
@@ -18,74 +18,74 @@ print("-="*50)
 print(" "*(50-(round(len(academia.nome)/2))),academia.nome)
 print("-="*50)
 
-while True:
-    menu = input('''\nDigite 1 para entrar na academia
+while True:                                                                                                 #Enquanto o programa não encerrado pelo usuário, repete linha 22-91                                                
+    menu = input('''\nDigite 1 para entrar na academia                                                               
 Digite 2 para sair da academia
 Digite 3 para cadastrar uma pessoa
 Digite 4 para editar uma pessoa
 Digite 5 para excluir uma pessoa
 Digite 6 para cadastrar um aparelho
-Digite 7 para encerrar o programa: ''')
+Digite 7 para encerrar o programa: ''')                                                                     #Entrada da variável menu
 
-    while menu not in '1234567':
-        print(f'\nNúmero inválido, tente novamente:')
-        menu = input('''\nDigite 1 para entrar na academia
+    while menu not in '1 2 3 4 5 6 7':                                                                      #Enquanto o número digitado pelo usuário não for uma das opções possíveis, repete linha 31-38
+        print(f'\nNúmero inválido, tente novamente:')                                                       #Caso entre no laço, imprime a mensagem "Número inválido, tente novamente:"
+        menu = input('''\nDigite 1 para entrar na academia                                                  
 Digite 2 para sair da academia
 Digite 3 para cadastrar uma pessoa
 Digite 4 para editar uma pessoa
 Digite 5 para excluir uma pessoa
 Digite 6 para cadastrar um aparelho
-Digite 7 para encerrar o programa: ''')    
+Digite 7 para encerrar o programa: ''')                                                                     #Nova entrada da variável menu
     
     
-    if menu == '1':
-        nomeEntrada = input('\nDigite o seu nome: ').capitalize()
-        academia.entrar(nomeEntrada)
+    if menu == '1':                                                                                         #Se a opção escolhida for = 1, tenta entrar na academia
+        nomeEntrada = input('\nDigite o seu nome: ').capitalize()                                           #Entrada de nome digitado pelo usuário
+        academia.entrar(nomeEntrada)                                                                        #Invoca o método para entrar na academia
 
-    elif menu == '2':
-        nomeSaida = input('\nDigite o seu nome: ').capitalize()
-        academia.sair(nomeSaida)
+    elif menu == '2':                                                                                       #Se a opção escolhida for = 2 tenta sair da academia
+        nomeSaida = input('\nDigite o seu nome: ').capitalize()                                             #Entrada de nome digitado pelo usuário
+        academia.sair(nomeSaida)                                                                            #Invoca o método para sair da academia
 
-    elif menu == '3':
-        nome = input('\nDigite o seu nome: ').capitalize()
-        cpf = input('Digite o seu CPF: ')
-        idade = int(input('Digite a sua idade: '))
-        while idade < 0:
-            print('\nNúmero inválido, a idade precisa ser maior do que 0')
-            idade = int(input('Digite a sua idade: '))        
+    elif menu == '3':                                                                                       #Se a opção escolhida for = 3, tenta cadastrar pessoa
+        nome = input('\nDigite o seu nome: ').capitalize()                                                  #Entrada de nome digitado pelo usuário
+        cpf = input('Digite o seu CPF: ')                                                                   #Entrada de CPF digitado pelo usuário
+        idade = int(input('Digite a sua idade: '))                                                          #Entrada de idade, número inteiro digitado pelo usuário
+        while idade < 0:                                                                                    #Enquanto a idade for < 0 repete linha 54-55
+            print('\nNúmero inválido, a idade precisa ser maior do que 0')                                  #Imprime mensagem "Número inválido, a idade precisa ser maior do que 0"
+            idade = int(input('Digite a sua idade[Deve ser um número inteiro > 0]: '))                      #Entrada de idade, número inteiro digitado pelo usuário
         
-        print("\nInforme 1 para o dia e turno que deseja frequentar a academia e 0 para os dias que não deseja: ")
-        for i in range(6):
-            for j in range(3):
-                matrizHorarios[i][j] = int(input(f'{listaDiasSemana[i]} de {listaTurnos[j]}: '))
-                while matrizHorarios[i][j] != 0 and matrizHorarios[i][j] != 1:
-                    print('\nNúemro inválido, utilize apenas 0 ou 1')
-                    matrizHorarios[i][j] = int(input(f'{listaDiasSemana[i]} de {listaTurnos[j]}: '))
+        print("\nInforme 1 para o dia e turno que deseja frequentar a academia e 0 para os dias que não deseja: ") #Imprime mensagem para indicar como preenche os horários
+        for i in range(6):                                                                                  #Repete 6 vezes linha 59-63
+            for j in range(3):                                                                              #Repete 3 vezes linha 60-63 
+                matrizHorarios[i][j] = int(input(f'{listaDiasSemana[i]} de {listaTurnos[j]}: '))            #Entrada de 0 ou 1 pelo usuário
+                while matrizHorarios[i][j] != 0 and matrizHorarios[i][j] != 1:                              #Enquanto a entrada for diferente de 0 e 1 repete linha 62-63
+                    print('\nNúemro inválido, utilize apenas 0 ou 1')                                       #Imprime mensagem "Núemro inválido, utilize apenas 0 ou 1"
+                    matrizHorarios[i][j] = int(input(f'{listaDiasSemana[i]} de {listaTurnos[j]}: '))        #Entrada de 0 ou 1 pelo usuário
                     
-        menuCadastro = input('''Digite 1 para cadastrar aluno \nDigite 2 para cadastrar professor: ''')
-        while menuCadastro != '1' and menuCadastro != '2':
-            print('\nNúemro inválido, utilize apenas 1 ou 2.Tente novamente')
-            menuCadastro = input('''Digite 1 para cadastrar aluno \nDigite 2 para cadastrar professor: ''')
+        menuCadastro = input('''Digite 1 para cadastrar aluno \nDigite 2 para cadastrar professor: ''')     #Entrada para a variável menuCadastro
+        while menuCadastro != '1' and menuCadastro != '2':                                                  #Enquanto a entrada =! de 1 ou 2 repete linha 67-68
+            print('\nNúemro inválido, utilize apenas 1 ou 2.Tente novamente')                               #Imprime mensagem "Núemro inválido, utilize apenas 1 ou 2"
+            menuCadastro = input('''Digite 1 para cadastrar aluno \nDigite 2 para cadastrar professor: ''') #Nova entrada para a variável menuCadastro
         
-        if menuCadastro == '1':
-            peso = float(input('Digite o seu peso em kg[Para separar a parte inteira da parte fracionária, utilize "."]: '))
-            altura = float(input('Digite a sua altura em m[Para separar a parte inteira da parte fracionária, utilize "."]: '))
-            objetivo = input('Digite o seu objetivo: ')
-            auxAluno = academia.cadastrarAluno(nome, idade, cpf, matrizHorarios, peso, altura, objetivo)
+        if menuCadastro == '1':                                                                                                 #Se a opção 1 for escolhida, cadastra aluno
+            peso = float(input('Digite o seu peso em kg[Para separar a parte inteira da parte fracionária, utilize "."]: '))    #Entrada de peso digitada pelo usuário
+            altura = float(input('Digite a sua altura em m[Para separar a parte inteira da parte fracionária, utilize "."]: ')) #Entrada de altura digitada pelo usuário
+            objetivo = input('Digite o seu objetivo: ')                                                                         #Entrada de objetivo digitada pelo usuário
+            auxAluno = academia.cadastrarAluno(nome, idade, cpf, matrizHorarios, peso, altura, objetivo)                        #Variável usada para invocar o método de cadastro de aluno
             
-        elif menuCadastro == '2':
-            numeroCTPS = input('Digite o número da sua carteira de trabalho: ')
-            auxProfessor = academia.cadastrarProfessor(nome, idade, cpf, matrizHorarios, numeroCTPS)
+        elif menuCadastro == '2':                                                                           #Se a opção 2 for escolhida, cadastra professor       
+            numeroCTPS = input('Digite o número da sua carteira de trabalho: ')                             #Entrada de número CTPS digitada pelo usuário
+            auxProfessor = academia.cadastrarProfessor(nome, idade, cpf, matrizHorarios, numeroCTPS)        #Variável usada para invocar o método de cadastro de professor
         
-    elif menu == '6':
-        nomeAparelho = input('\nDigite o nome do aparelho: ').capitalize()
-        restricaoIdade = bool(input('Digite 0 se o aparelho puder ser usado por menores de 16 anos ou 1 se não puder: '))
-        while restricaoIdade != 0 and restricaoIdade != 1:
-                print('\nNúemro inválido, utilize apenas 0 ou 1')
-                restricaoIdade = bool(input('Digite 0 se o aparelho puder ser usado por menores de 16 anos ou 1 se não puder: '))
+    elif menu == '6':                                                                                                              #Se a opção 6 for escolhida, cadastra Aaparelho
+        nomeAparelho = input('\nDigite o nome do aparelho: ').capitalize()                                                         #Entrada de nome do aparelho digitada pelo usuário
+        restricaoIdade = bool(input('Digite 0 se o aparelho puder ser usado por menores de 16 anos ou 1 se não puder: '))          #Entrada de 0 ou 1 para indicar se o aparelho tem restrição de idade
+        while restricaoIdade != 0 and restricaoIdade != 1:                                                                         #Equanto entrada =! de 0 ou 1 repete linha 84-85
+                print('\nNúemro inválido, utilize apenas 0 ou 1')                                                                  #Imprime mensagem "Núemro inválido, utilize apenas 0 ou 1"
+                restricaoIdade = bool(input('Digite 0 se o aparelho puder ser usado por menores de 16 anos ou 1 se não puder: '))  #Nova entrada de 0 ou 1 para indicar se o aparelho tem restrição de idade
                 
-        auxAparelho = academia.cadastrarAparelho(nomeAparelho,restricaoIdade)
+        auxAparelho = academia.cadastrarAparelho(nomeAparelho,restricaoIdade)                               #Variável usada para invocar método de cadastro de aparelho
         
-    elif menu == '7':
-        print('\nFIM !')
-        break
+    elif menu == '7':                                                                                       #Se a opção for 7, encerra o programa
+        print('\nFIM !')                                                                                    #Imprime mensagem de encerramento do programa 
+        break                                                                                               #Quebra do laço do programa
